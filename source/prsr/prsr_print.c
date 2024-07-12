e34efbc2d21288c33078cad068b1e9fad03dddfc
Lucas Opoka <lucasopoka@icloud.com>
2024-07-12 17:55:44 +0300

    Reworked prsr print to be more concise

--- source/prsr/prsr_print.c
+++ source/prsr/prsr_print.c
@@ -6,7 +6,7 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 11:32:37 by lopoka            #+#    #+#             */
-/*   Updated: 2024/07/12 16:50:09 by lucas            ###   ########.fr       */
+/*   Updated: 2024/07/12 17:52:04 by lucas            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../../include/minishell.h"
@@ -27,32 +27,13 @@ void	ft_print_expanded(t_ast *ast)
 
 void	ft_print_ast(t_ms *ms, t_ast *ast, int expd)
 {
-	t_ast	*curr;
-
 	if (!ast)
 		return ;
 	if (!expd)
-	{
-		printf("Ast type %d, str %s", ast->type, ast->str);
-		if (!ast->io)
-			printf("\n");
-		else
-		{
-			curr = ast->io;
-			while (curr)
-			{
-				printf(", IO type: %d, s: %s", curr->type, curr->str);
-				curr = curr->io;
-			}
-			printf("\n");
-		}
-	}
+		printf("Ast type %d, str %s\n", ast->type, ast->str);
 	if (expd && ast->expd_str)
 		ft_print_expanded(ast);
-	if (ast->io)
-		ft_print_ast(ms, ast->io, expd);
-	if (ast->left)
-		ft_print_ast(ms, ast->left, expd);
-	if (ast->right)
-		ft_print_ast(ms, ast->right, expd);
+	ft_print_ast(ms, ast->io, expd);
+	ft_print_ast(ms, ast->left, expd);
+	ft_print_ast(ms, ast->right, expd);
 }
