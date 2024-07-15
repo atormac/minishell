a441cf3c48384bfa147913e912d966a9b8728ee2
atormac <atorma@student.hive.fi>
2024-07-15 18:53:40 +0300

    export without args prints env in sorted order

--- source/builtin/export.c
+++ source/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 18:19:33 by atorma            #+#    #+#             */
-/*   Updated: 2024/07/15 18:20:31 by atorma           ###   ########.fr       */
+/*   Updated: 2024/07/15 18:52:59 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,56 @@
 #include "../../include/builtin.h"
 #include "../../include/environment.h"
 
-static void	export_print(char **env)
+static void	env_sort(char **env)
+{
+	size_t	i;
+	size_t	j;
+	size_t	size;
+	char	*tmp;
+
+	i = 0;
+	size = args_count(env);
+	while (i < size)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (ft_strcmp(env[i], env[j]) > 0)
+			{
+				tmp = env[i];
+				env[i] = env[j];
+				env[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+static int	export_print(char **env)
 {
 	size_t	i;
 	char	*eq;
+	char	**sorted;
 
 	i = 0;
-	while (env[i])
+	sorted = env_clone(env);
+	if (!sorted)
+		return (1);
+	env_sort(sorted);
+	while (sorted[i])
 	{
-		eq = ft_strchr(env[i], '=');
+		eq = ft_strchr(sorted[i], '=');
 		if (eq)
 		{
 			ft_putstr_fd("declare -x ", STDOUT_FILENO);
-			write(STDOUT_FILENO, env[i], (eq - env[i]) + 1);
+			write(STDOUT_FILENO, sorted[i], (eq - sorted[i]) + 1);
 			printf("\"%s\"\n", eq + 1);
 		}
 		i++;
 	}
+	free_array(sorted);
+	return (0);
 }
 
 int	builtin_export(t_ms *ms, char **args)
@@ -40,10 +73,7 @@ int	builtin_export(t_ms *ms, char **args)
 
 	i = 0;
 	if (args[0] == NULL)
-	{
-		export_print(ms->env);
-		return (0);
-	}
+		return (export_print(ms->env));
 	while (args[i])
 	{
 		val = ft_strchr(args[i], '=');
