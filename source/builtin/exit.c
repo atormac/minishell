a17c2c733808c6eb3666ff119646e9bc976732ac
atormac <atorma@student.hive.fi>
2024-07-25 19:37:37 +0300

    Exit overflow check

--- source/builtin/exit.c
+++ source/builtin/exit.c
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 19:47:40 by atorma            #+#    #+#             */
-/*   Updated: 2024/07/25 19:14:38 by atorma           ###   ########.fr       */
+/*   Updated: 2024/07/25 19:36:06 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,26 @@ static int	is_numeric(char *s)
 	return (1);
 }
 
+static int	is_overflow(char *s)
+{
+	size_t	len;
+	int		res;
+
+	if (*s == '-')
+		s++;
+	len = ft_strlen(s);
+	if (len >= (sizeof("9223372036854775807") - 1))
+	{
+		if (ft_strcmp(s, "9223372036854775807") > 0)
+		{
+			res = ft_atoi(s);
+			if (res == -1 || res == 0)
+				return (1);
+		}
+	}
+	return (0);
+}
+
 int	builtin_exit(t_ms *ms, char **args)
 {
 	int	code;
@@ -36,7 +56,7 @@ int	builtin_exit(t_ms *ms, char **args)
 		ft_putstr_fd("exit\n", STDERR_FILENO);
 	if (args[0] == NULL)
 		return (ms->exit_code);
-	if (!is_numeric(args[0]))
+	if (!is_numeric(args[0]) || is_overflow(args[0]))
 	{
 		error_builtin("exit", args[0], "numeric argument required");
 		return (2);
